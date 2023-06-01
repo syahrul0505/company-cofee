83e30d8b6750162f6e541ed45aae75d2173a230f
harrymuliawan03 <harrymuliawan03@gmail.com>
2023-06-02 00:14:10 +0700

    fitur validasi input message

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,17 @@
 const send = document.getElementById('send');
 const message = document.getElementById('message');
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
+const inputMesage = document.getElementById('input-message');
 
-send.addEventListener('click', () => {
-    message.reset();
+send.addEventListener('click', (e) => {
+    if(inputName.value != '' && inputEmail.value != '' && inputMesage.value != ''){
+        send.setAttribute('href', '#ex1');
+        send.setAttribute('rel', 'modal:open');
+        message.reset();
+    }else {
+        alert('Form input message tidak boleh ada yang kosong !');
+        send.setAttribute('href', '#');
+        send.setAttribute('rel', '');
+    }
 });
